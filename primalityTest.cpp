e6f37f7078cb5fe69428428552fa35a8069bf60b
raam55 <rishabhchaturvedi579@gmail.com>
2023-09-11 17:28:34 +0530

    primalityTest.cpp

---
+++ primalityTest.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+bool isPrime(int n){
+    if(n == 1) return false;
+    for(int i = 2;(i * i) <= n;i++){   
+        if(n % i == 0){
+            return false;
+        };
+    }
+    return true;
+}
+
+int main() {
+	// your code goes here
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int t;
+    cin >> t;
+    while(t--){
+    int n;
+    cin >> n;
+    if(isPrime(n)) cout << "yes" << "\n";
+    else cout << "no" << "\n";
+    }
+	return 0;
+}
