420900e035778ce4b58d08e59dd8aa3337802555
raam55 <rishabhchaturvedi579@gmail.com>
2023-09-11 17:32:10 +0530

    distinctColors

---
+++ distinctColors.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int t; 
+    cin >> t;
+    while(t--){
+    int n;
+    cin >> n;
+    int arr[n];
+    for(int i = 0;i < n;i++) cin >> arr[i];
+    int *maxi = max_element(arr,arr + n);
+    cout << *maxi << "\n";
+    }
+	return 0;
+}
