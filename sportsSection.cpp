aa3893fad57096a3047c2019004dd16c218f4f66
raam55 <rishabhchaturvedi579@gmail.com>
2023-09-27 21:20:09 +0530

    sportsSection

---
+++ sportsSection.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int x;
+    cin >> x;
+    if(x > 7 && x < 11) cout << "YES" << "\n";
+    else cout << "NO" << "\n";
+ 	return 0;
+}
