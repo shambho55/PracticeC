6d4bec9a0c559d399e594c431e1943fa290c8abc
raam55 <rishabhchaturvedi579@gmail.com>
2023-09-27 21:21:14 +0530

    sellingInsurance

---
+++ sellingInsurance.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    
+	// your code goes here
+	int t ;
+    cin >> t;
+    while(t--){
+    int x;
+    cin >> x;
+    cout << ceil(100 / (0.2 * x)) << "\n";
+    }
+	return 0;
+}
