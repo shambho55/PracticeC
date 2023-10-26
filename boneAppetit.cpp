5537187d8f4538cd2e636ac6b76be4741cff499b
raam55 <rishabhchaturvedi579@gmail.com>
2023-10-26 05:52:16 +0530

    boneAppetit

---
+++ boneAppetit.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int n1,m1,n2,m2;
+    cin >> n1 >> m1;
+    cin >> n2 >> m2;    
+    cout << ((n1*n2) + (m1*m2));
+	return 0;
+}
