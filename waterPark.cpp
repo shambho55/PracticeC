741bffb94ebd9367d4a23058969754d297a44da7
raam55 <rishabhchaturvedi579@gmail.com>
2023-11-30 11:42:33 +0530

    waterPark

---
+++ waterPark.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int w,h;
+	cin >> w >> h;
+	if(w >= 60 && h <= 130) cout << "Yes" << "\n";
+    else cout << "No" << "\n";
+	return 0;
+}
